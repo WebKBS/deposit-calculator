f93732c946fb0608a6a5cc806e3c8ddca033ace7
gangbeomsu <kbs21c1987@gmail.com>
2024-01-21 21:23:38 +0900

    폰트 변경 및 input 확대 금지

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex-1">
       <div className="max-w-screen-md px-6 pt-8 m-auto">
-        <h2 className="mb-6 text-2xl">SH 보증금 임대료 계산기</h2>
+        <h2 className="mb-6 text-2xl font-bold">SH 보증금 임대료 계산기</h2>
         <p className="text-xs text-right text-green-600 dark:text-yellow-300">
           *빨간색 부분만 입력하세요.
         </p>
