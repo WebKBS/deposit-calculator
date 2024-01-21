5be6e9cfdfaf3502adf2c95725936242d86e6439
gangbeomsu <kbs21c1987@gmail.com>
2024-01-21 16:35:11 +0900

    잔금 작업중

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,15 +1,46 @@
 'use client';
+import { parseInputValue, removeComma } from '@/lib/utils';
 import { ChangeEvent, useState } from 'react';
 import { TipAlertDialog } from './TipAlertDialog';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 
 export default function Calculator() {
-  const [defaultValue, setDefault] = useState<number | null>();
+  const [defaultValue, setDefault] = useState<string | null>();
+  const [downPayment, setDownPayment] = useState<string | null>();
+  const [calcDownPayment, setCalcDownPayment] = useState<string | null>();
+  const [balance, setBalance] = useState<string | null>();
+  const [calcBalance, setCalcBalance] = useState<string | null>();
+
+  // 기본 보증금
   const defaultHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setDefault(Number(event.target.value));
-    console.log(defaultValue);
+    const inputNumber = parseInputValue(event.target.value);
+    setDefault(inputNumber.toLocaleString());
+    setDownPayment('');
+    setCalcDownPayment('');
+    // console.log(inputNumber);
   };
+
+  // 계약금
+  const downPaymentHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!defaultValue) return alert('기본 보증금을 입력해주세요.');
+
+    const inputNumber = event.target.value;
+    const removeCommaDefault = +removeComma(defaultValue!); // 기본 보증금 콤마 제거
+    const removeCommaDownPayment = +removeComma(calcDownPayment!); // 계약금 콤마 제거
+    const percentNumver = +inputNumber / 100; // 계약금 퍼센트
+
+    setDownPayment(inputNumber);
+    console.log(removeCommaDownPayment);
+
+    setCalcDownPayment((removeCommaDefault! * +percentNumver).toLocaleString());
+    setCalcBalance(
+      (removeCommaDefault - removeCommaDownPayment).toLocaleString()
+    );
+
+    setBalance(0 || (100 - +inputNumber!).toString());
+  };
+
   return (
     <div>
       <div className="mb-4">
@@ -21,14 +52,71 @@ export default function Calculator() {
         </div>
         <Input
           id="defaultValue"
-          type="number"
-          placeholder="Enter a number"
+          type="text"
+          placeholder="기본 보증금 입력"
           onChange={defaultHandler}
           value={defaultValue || ''}
+          pattern="[0-9]*"
+          inputMode="numeric"
+          className="text-right"
         />
       </div>
-      <div>
-        <Input type="number" placeholder="Enter a number" />
+      <div className="mb-4">
+        <div className="mb-2">
+          <Label htmlFor="downPayment" className="mr-2">
+            계약금
+          </Label>
+          <TipAlertDialog title="계약금이란?" body="" />
+        </div>
+        <div className="flex gap-4">
+          <div className="flex items-center gap-1">
+            <Input
+              id="downPayment"
+              type="text"
+              placeholder="계약금"
+              onChange={downPaymentHandler}
+              value={downPayment || ''}
+              pattern="[0-9]*"
+              inputMode="numeric"
+              className="flex-1 w-20 text-right"
+              maxLength={3}
+            />
+            <span>%</span>
+          </div>
+          <Input
+            readOnly
+            className="text-right"
+            placeholder="계약 금액"
+            value={calcDownPayment || ''}
+          />
+        </div>
+      </div>
+      <div className="mb-4">
+        <div className="flex items-center justify-between mb-2">
+          <Label htmlFor="balance" className="mr-2">
+            잔금
+          </Label>
+          <p className="text-xs">*계약금을 입력하면 자동 입력됩니다.</p>
+        </div>
+        <div className="flex gap-4">
+          <div className="flex items-center gap-1">
+            <Input
+              id="balance"
+              type="text"
+              placeholder="잔금"
+              value={balance || ''}
+              className="flex-1 w-20 text-right"
+              readOnly
+            />
+            <span>%</span>
+          </div>
+          <Input
+            readOnly
+            className="text-right"
+            placeholder="잔금"
+            value={calcBalance || ''}
+          />
+        </div>
       </div>
       <div>
         <Input type="number" placeholder="Enter a number" />
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * 1000단위 콤마 추가
+ * @param value - 값
+ * @returns
+ */
+export const parseInputValue = (value: string): number => {
+  return parseFloat(value?.replace(/,/g, '')) || 0;
+};
+
+// 콤마 제거
+export const removeComma = (value: string) => {
+  return value.replace(/,/g, '');
+};
