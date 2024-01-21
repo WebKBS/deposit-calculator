14c48029dbe583fc7d99e9015f8e0fc7e06a86c2
gangbeomsu <kbs21c1987@gmail.com>
2024-01-21 21:20:46 +0900

    오타수정

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="max-w-screen-md px-6 pt-8 m-auto">
         <h2 className="mb-6 text-2xl">SH 보증금 임대료 계산기</h2>
         <p className="text-xs text-right text-green-600 dark:text-yellow-300">
-          *빨간색 부부만 입력하세요.
+          *빨간색 부분만 입력하세요.
         </p>
         <Calculator />
       </div>
