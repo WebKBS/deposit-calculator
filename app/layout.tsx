e7f678abbd7f4484c2d44e1ab62893232f41229e
gangbeomsu <kbs21c1987@gmail.com>
2024-01-23 00:28:08 +0900

    vercel 애널리틱스 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/header/Header';
 import { ThemeProvider } from '@/components/theme/theme-provider';
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -39,6 +40,7 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
+            <Analytics />
           </div>
         </ThemeProvider>
       </body>
