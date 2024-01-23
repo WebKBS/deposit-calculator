29ed63da1b2c720b00787f06819922188118f833
gangbeomsu <kbs21c1987@gmail.com>
2024-01-23 20:40:08 +0900

    robots 하나로 변경, nextconfig설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,6 @@ const withPWA = withPWAInit({
   aggressiveFrontEndNavCaching: true, // <link rel="stylesheet"> 및 <script> 태그를 캐시한다.
   reloadOnOnline: true, // 앱이 온라인 상태로 전환될 때 새로고침한다.
   disable: false, // PWA 비활성화 여부
-  workboxOptions: {
-    disableDevLogs: true, // 개발 모드에서 로그를 활성화한다.
-  },
 });
 
 const nextConfig = {};
