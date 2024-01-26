2dc181a74f40e240c951a5e27bec701a05824727
gangbeomsu <kbs21c1987@gmail.com>
2024-01-26 17:11:44 +0900

    푸터 내용수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,22 @@
+import { HiOutlineMail } from 'react-icons/hi';
+
 export default function Footer() {
   return (
     <footer className="p-4 mt-auto border-t">
+      <div className="flex flex-wrap items-center justify-center gap-2 max-w-3xl w-full mx-auto mb-2">
+        <div className="text-xs text-center">
+          불편한점 또는 개선사항이 있으시면 이메일로 연락주세요.
+        </div>
+        <div className="text-xs text-center">
+          <a
+            href="mailto:kbs21c1987@gmail.com"
+            className="flex items-center gap-1 justify-center"
+          >
+            <HiOutlineMail size={16} />
+            kbs21c1987@gmail.com
+          </a>
+        </div>
+      </div>
       <div className="text-xs text-center">
         Copyright &copy; [Dev Kang]. All rights reserved.
       </div>
