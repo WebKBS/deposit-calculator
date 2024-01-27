64159d4f1d981f6148af32ffc3e00d244e82d617
gangbeomsu <kbs21c1987@gmail.com>
2024-01-28 02:14:22 +0900

    의미없다 안된다.

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -463,7 +463,7 @@ export default function Calculator() {
           id="defaultDeposit"
           type="text"
           placeholder="기본 보증금 입력"
-          pattern="\d*"
+          pattern="[0-9]*"
           inputMode="numeric"
           className="text-right border-red-500"
           ref={defaultInput}
