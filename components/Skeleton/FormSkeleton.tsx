60ef1eb925196452ae15813a78c83b8d7d7b7f89
gangbeomsu <kbs21c1987@gmail.com>
2024-01-28 22:40:06 +0900

    스켈레톤 수정

--- components/Skeleton/FormSkeleton.tsx
+++ components/Skeleton/FormSkeleton.tsx
@@ -1,17 +1,27 @@
-import { Skeleton } from '../ui/skeleton';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function FormSkeleton() {
   return (
     <div>
-      <div className="flex items-center space-x-4">
-        <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="flex items-center space-x-4 mt-4">
         <div className="space-y-2">
           <Skeleton className="h-4 w-[250px]" />
           <Skeleton className="h-4 w-[200px]" />
         </div>
       </div>
-      <div className="flex items-center space-x-4">
-        <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="flex items-center space-x-4 mt-4">
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4 mt-4">
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4 mt-4">
         <div className="space-y-2">
           <Skeleton className="h-4 w-[250px]" />
           <Skeleton className="h-4 w-[200px]" />
