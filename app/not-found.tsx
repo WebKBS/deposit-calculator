d55057b045b10cb8a1fd6dbbec24bee397557fba
gangbeomsu <kbs21c1987@gmail.com>
2024-01-28 21:52:05 +0900

    404 로티 파일 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import NotFoundLottie from '@/components/Error/NotFoundLottie';
+
+export default function NotFound() {
+  return (
+    <main className="flex justify-center flex-1 items-center">
+      <NotFoundLottie />
+    </main>
+  );
+}
